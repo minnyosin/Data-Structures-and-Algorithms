a88caa1c7fae7b33ac4b64b8a4c1084afded2c66
minnyosin <minnyosin.dev@gmail.com>
2026-07-02 14:24:34 +0800

    initial commit

---
+++ Chapter6CA/Chapter6CA/Chapter6CA.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+template <class T>
+class DoubleNodeType
+{
+public:
+	T info;
+	DoubleNodeType<T>* next;
+	DoubleNodeType<T:
+};
+int main()
+{
+	return 0;
+}
